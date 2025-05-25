8831f28a9afa042426b41954f616d6bed53bc7c1
FlockNation <seshrohan@gmail.com>
2025-05-25 17:21:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 fetch('posts.json')
-  .then(res => res.json())
+  .then(response => response.json())
   .then(posts => {
     const featuredContainer = document.getElementById('featured-container');
     const latestContainer = document.getElementById('latest-container');
 
-    posts.forEach(post => {
-      const card = document.createElement('a');
-      card.className = 'post-card';
-      card.href = post.link;
-      card.target = '_blank';
-      card.innerHTML = `
-        <div class="post-image">
-          <img src="${post.image}" alt="${post.title}">
-        </div>
-        <div class="post-content">
-          <h2 class="post-title">${post.title}</h2>
-          <p class="post-description">${post.description}</p>
-          <p class="post-meta">${post.date} • ${post.author}</p>
-        </div>
-      `;
+    const featuredPost = posts.find(post => post.featured);
+    const latestPosts = posts.filter(post => !post.featured);
 
-      if (post.featured) {
-        featuredContainer.appendChild(card);
-      } else {
-        latestContainer.appendChild(card);
-      }
+    if (featuredPost) {
+      featuredContainer.innerHTML = generatePostHTML(featuredPost, true);
+    }
+
+    latestPosts.forEach(post => {
+      latestContainer.innerHTML += generatePostHTML(post, false);
     });
   });
+
+function generatePostHTML(post, isFeatured) {
+  return `
+    <a class="post-card" href="${post.url}" target="_blank">
+      <div class="post-image">
+        <img src="${post.image}" alt="${post.title}" onerror="this.src='images/default.png'">
+      </div>
+      <div class="post-content">
+        <div class="post-title">${post.title}</div>
+        <div class="post-description">${post.description}</div>
+        <div class="post-meta">${post.date} • ${post.author}</div>
+      </div>
+    </a>
+  `;
+}
