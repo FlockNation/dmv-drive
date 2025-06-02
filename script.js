babdb45d19a402192761706cc274b4262d2a66e9
FlockNation <seshrohan@gmail.com>
2025-06-01 22:21:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,15 +22,17 @@ fetch('https://api.rss2json.com/v1/api.json?rss_url=https://dmvdrive.substack.co
   });
 
 function generatePostHTML(post, isFeatured) {
+  const imageUrl = post.image || extractImageFromContent(post.content) || 'images/default.png';
+
   return `
-    <a class="post-card ${isFeatured ? 'featured' : ''}" href="${post.link}" target="_blank">
+    <a class="post-card" href="${post.url}" target="_blank">
       <div class="post-image">
-        <img src="${post.thumbnail || 'images/default.png'}" alt="${post.title}" onerror="this.src='images/default.png'">
+        <img src="${imageUrl}" alt="${post.title}" onerror="this.src='images/default.png'">
       </div>
       <div class="post-content">
         <div class="post-title">${post.title}</div>
-        <div class="post-description">${stripHTML(post.description).slice(0, 160)}...</div>
-        <div class="post-meta">${new Date(post.pubDate).toLocaleDateString()} • ${post.author || 'DMV Drive'}</div>
+        <div class="post-description">${post.description}</div>
+        <div class="post-meta">${post.date} • ${post.author}</div>
       </div>
     </a>
   `;
@@ -40,4 +42,11 @@ function stripHTML(html) {
   const div = document.createElement("div");
   div.innerHTML = html;
   return div.textContent || div.innerText || "";
+}
+
+function extractImageFromContent(htmlContent) {
+  const div = document.createElement('div');
+  div.innerHTML = htmlContent;
+  const img = div.querySelector('img');
+  return img ? img.src : null;
 }
