a8016dc545b572ac37d942194533422ea03c610b
FlockNation <seshrohan@gmail.com>
2025-06-02 00:30:51 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__, static_folder='static')
 
-SUBSTACK_API = "https://dmvdrive.substack.com/api/v1/posts?limit=60"
+SUBSTACK_API = "https://dmvdrive.substack.com/api/v1/posts?limit=50"
 
 @app.route('/api/posts')
 def get_posts():
